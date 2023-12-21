f93514a11320bad563ffff6c43d4840cb28df40f
Nathan <antnefedov33@gmail.com>
2023-12-21 16:18:36 +0300

    wrote the script for the analog clock

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,28 @@
 "use strict"
+
+//clock hands moving
+const hr = document.querySelector('.hour');
+const mn = document.querySelector('.min');
+const sc = document.querySelector('.sec');
+
+const hourDeg = 360 / 12;
+const minDeg = 360 / 60;
+const secDeg = 360 / 60;
+
+setInterval(() => {
+    let day = new Date();
+    let hours = day.getHours();
+    let minutes = day.getMinutes();
+    let seconds = day.getSeconds();
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    let hh = hours * hourDeg;
+    let mm = minutes * minDeg;
+    let ss = seconds * secDeg;
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+});
